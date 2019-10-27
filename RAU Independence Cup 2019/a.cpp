20265a24c7c677fd0949573aca9ec0f7c6566001
kantuni <henrikh.kantuni@gmail.com>
2019-10-27 15:02:38 +0400

    Update a.cpp

--- RAU Independence Cup 2019/a.cpp
+++ RAU Independence Cup 2019/a.cpp
@@ -8,10 +8,6 @@ int main() {
   for (int i = 0; i < n; i++) {
     cin >> a[i];
   }
-  if (a.size() == 1) {
-    cout << 0 << endl;
-    return 0;
-  }
   vector<long long> b(a);
   long long ans = 0;
   for (int i = b.size() - 2; i > -1; i--) {
@@ -21,24 +17,19 @@ int main() {
       ans += diff + 1;
     }
   }
-  long long diff = b[1] - b[0];
-  if (diff >= 0) {
-    b[0] += diff + 1;
-    ans += diff + 1;
-  }
-  long long c = ans, cd = 0;
+  long long c = ans, diff = 0;
   for (int i = 1; i < b.size(); i++) {
     long long bpp = i > 1 ? b[i - 2] : -1;
-    cd = max(bpp + 1, a[i - 1]) - b[i - 1];
-    b[i - 1] += cd;
-    c += cd;
+    diff = max(bpp + 1, a[i - 1]) - b[i - 1];
+    b[i - 1] += diff;
+    c += diff;
     long long bn = i + 1 < b.size() ? b[i + 1] : 0;
     long long newb = max({b[i - 1] + 1, a[i], bn + 1});
-    cd = newb - b[i];
-    b[i] += cd;
-    c += cd;
+    diff = newb - b[i];
+    b[i] += diff;
+    c += diff;
     ans = min(ans, c);
   }
   cout << ans << endl;
   return 0;
-}
+
